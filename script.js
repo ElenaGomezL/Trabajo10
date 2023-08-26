4d5ef26e7a24c9bb43b74ae659429ca6ef3ae3ed
María Elena Gomez Lezcano <105325361+ElenaGomezL@users.noreply.github.com>
2023-08-26 14:20:30 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var nombre= "Elena" //string
+var apellido= "Gomez" //string
+console.log(nombre + " " + apellido);
+var num1 = 1 //number
+var num2 = 5//number
+var suma = num1+num2//number
+console.log(suma);
+
+function suma2(suma,num3){
+    var suma2=suma+num3;
+    console.log(suma2)
+}
+suma2(suma,2)
